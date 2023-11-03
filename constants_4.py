5213aa8da29bc159e6ce14915e7e5db3b8b65543
simonabuetti <149421232+simonabuetti@users.noreply.github.com>
2023-11-03 17:07:54 -0500

    Update constants_4.py

--- constants_4.py
+++ constants_4.py
@@ -1,22 +1,5 @@
-#* Copyright 2023 The Board of Trustees of the University of Illinois. All Rights Reserved.
-#
-# * Licensed under the terms of the Apache License 2.0 license (the "License")
-#
-# * The License is included in the distribution as License.txt file.
-#
-# * You may not use this file except in compliance with the License.
-#
-# * Software distributed under the License is distributed on an "AS IS" BASIS,
-#
-# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#
-# * See the License for the specific language governing permissions and limitations under the License.
-
-
-
 # * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 # Parameters for the Model of V1-based salience
-# Developed and written by Rachel F Heaton and John E Hummel
 # * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
 
